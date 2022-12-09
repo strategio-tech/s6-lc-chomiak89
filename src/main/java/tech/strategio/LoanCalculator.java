aaa0d0d875da5917d78db65eae6ea186e6f4b522
Kamil Chomiak <kchomiak@outlook.com>
2022-12-09 12:46:45 -0500

    fix javadoc, complete rewrite the method

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -7,13 +7,22 @@ public class LoanCalculator {
     /**
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
+     * <p>
+     *     This method takes a total loan amount, calculates and returns the amount still owed if
+     *     paying 10% of the remaining loan amount each month for 3 months.
+     * </p>
      * 
-     * @param ???
-     * @return ???
+     * @param amount the total amount of loan owed
+     * @return the amount still owed after paying off 10% of the balance each month for 3 months
      */
     static int getRemainingAmountIn3Months(int amount) {
         // TODO: Rewrite this method
-        return -1;
+        int numOfMonths = 3;
+        int remainingAmount = amount;
+        for (int i = 0; i < numOfMonths; i++ ) {
+            remainingAmount = (remainingAmount - (int)(remainingAmount * 0.1));
+        }
+        return remainingAmount;
     }
 
     public static void main(String[] args) {
